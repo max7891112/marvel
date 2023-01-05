2c8476d837e0fbe182ea0b1b14e9f1cba2a4971f
max7891112 <max.alkhimovich@gmail.com>
2023-01-05 15:54:32 +1000

    3 lesson is over

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('nice')
+import 'regenerator-runtime/runtime';
+import { API_URL_BASE,URL_COMICS } from './constants/api';
+import { getDataApi } from './utils/GetDataApi';
+(async () => {
+    const data = await getDataApi.getData(API_URL_BASE + URL_COMICS);
+    console.log('result:', data);
+})();
